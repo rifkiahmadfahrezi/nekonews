bb09b41fbac69c714c11e1400e9ab650411bf535
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-05-13 10:05:51 +0700

    ini payload CMS and customize style

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,9 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { Blogs } from './collections/Blogs'
+import { Reactions } from './collections/Reactions'
+import { Categories } from './collections/Categories'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,7 +23,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, Blogs, Reactions, Categories],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
